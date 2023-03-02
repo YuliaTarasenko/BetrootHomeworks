7a66082c7161deb8de9b61cdd3d5ab8cbbca8c13
YuliaTarasenko <juliatarasenko2@gmail.com>
2023-03-02 15:20:37 +0200

    hometask1

---
+++ hometask1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Hello, World!");
+string greetings = "Hello you!";
+Console.WriteLine(greetings);
+Console.WriteLine($"greetings contain {greetings.Length} letters.");
