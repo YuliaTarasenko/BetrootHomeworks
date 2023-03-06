3751a17e3b9593940e2d3d508d67155073d81030
YuliaTarasenko <juliatarasenko2@gmail.com>
2023-03-06 17:44:47 +0200

    new project

---
+++ hometask3/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Enter x");
+var stringX = Console.ReadLine();
+int.TryParse(stringX, out var x);
+Console.WriteLine("Enter y");
+var stringY = Console.ReadLine();
+int.TryParse(stringY, out var y);
+int Sum = 0;
+Console.WriteLine();
+
+if (x == y)
+{
+    Sum = x;
+    Console.WriteLine("x = y = " + Sum);
+}
+else
+{
+    int min = Math.Min(x, y);
+    int max = Math.Max(x, y);
+    for (int i = min; i <= max; i++)
+    {
+        Console.WriteLine(i);
+        Sum += i;
+    }
+    Console.WriteLine();
+    Console.WriteLine("The sum of numbers between x and y = " + Sum);
+}
+
+
+
