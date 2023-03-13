e657d49ac1bd1b038c7c816d0fc6b4ca1db0822b
YuliaTarasenko <juliatarasenko2@gmail.com>
2023-03-13 14:15:43 +0200

    Sorts

--- Hometask5/Program.cs
+++ Hometask5/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Hometask5
+﻿using System.Drawing;
+
+namespace Hometask5
 {
     internal class Program
     {
@@ -6,12 +8,14 @@
         {
             int[] array1 = CreateRandArray(10);
             DisplayArray(array1);
-            //Array.Sort(array1);
-            //SelectionSort(array1);
+            SelectionSort(array1);
             BubbleSort(array1);
+            InsertionSort(array1);
             DisplayArray(array1);
-            //int[,] array2 = new int[3,4];
-            //int[,,] array3 = new int[2,3,4];
+            int[,] array2 = CreateRandArray(3, 4);
+            DisplayArray(array2);
+            int[,,] array3 = CreateRandArray(2, 3, 4);
+            DisplayArray(array3);
         }
 
 
@@ -24,16 +28,79 @@
             Console.WriteLine();
         }
 
+        static void DisplayArray(int[,] array)
+        {
+            for (var i = 0; i < array.GetLength(0); i++)
+            {
+                for (var j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j]+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        static void DisplayArray(int[,,] array)
+        {
+            for (var i = 0; i < array.GetLength(0); i++)
+            {
+                for (var j = 0; j < array.GetLength(1); j++)
+                {
+                    for (var k = 0; k < array.GetLength(2); k++)
+                    {
+                        Console.Write(array[i, j, k] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
         static int[] CreateRandArray(int size)
         {
-            int[] ints = new int[size];
+            int[] array = new int[size];
             Random random = new Random();
             for (int i = 0; i < size; i++)
             {
-                ints[i] = random.Next(100);
+                array[i] = random.Next(100);
             }
 
-            return ints;
+            return array;
+        }
+
+        static int[,] CreateRandArray(int size1, int size2)
+        {
+            int[,] array = new int[size1,size2];
+            Random random = new Random();
+            for (int i = 0; i < size1; i++)
+            {
+                for (int j = 0; j < size2; j++)
+                {
+                    array[i,j] = random.Next(100);
+                }
+            }
+
+            return array;
+        }
+
+        static int[,,] CreateRandArray(int size1, int size2, int size3)
+        {
+            int[,,] array = new int[size1, size2,size3];
+            Random random = new Random();
+            for (int i = 0; i < size1; i++)
+            {
+                for (int j = 0; j < size2; j++)
+                {
+                    for (int k = 0; k < size3; k++)
+                    {
+                        array[i, j, k] = random.Next(100); 
+                    }
+                }
+            }
+
+            return array;
         }
 
         static void SelectionSort(int[] array)
@@ -41,14 +108,14 @@
             int size = array.Length;
             for(int i = 0; i < size - 1; i++ )
             {
-                int min_index = i;
+                int minIndex = i;
                 for(int j = i + 1; j < size; j++ )
                 {
-                    if (array[j] < array[min_index] )
-                        min_index = j;                    
+                    if (array[j] < array[minIndex] )
+                        minIndex = j;                    
                 }
-                int temp = array[min_index];
-                array[min_index] = array[i];
+                int temp = array[minIndex];
+                array[minIndex] = array[i];
                 array[i] = temp;
             }
         }
@@ -67,6 +134,23 @@
                         array[j + 1] = temp;
                     }
                 }
+            }
+        }
+
+        static void InsertionSort(int[] array)
+        {
+            int size = array.Length;
+            for (int i = 1; i < size; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+                while (j >= 0 && array[j] > key)
+                {
+                    array[j+1] = array[j];
+                    j = j - 1;
+                }
+
+                array[j + 1] = key;
             }
         }
     }
